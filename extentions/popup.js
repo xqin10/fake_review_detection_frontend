804789fe1b47a5f421663f7830eb7f6ee6507def
xqin10 <72183198+xqin10@users.noreply.github.com>
2021-05-12 12:07:04 +0800

    Dev iteration 3

--- extentions/popup.js
+++ extentions/popup.js
@@ -1,24 +1,23 @@
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  let url = tabs[0].url;
-  var patt = /(www.tripadvisor.com.*\/(Hotel_Review-|Restaurant_Review-))|(www.yelp.com.*)/g;
-  if (patt.test(url)) {
-    document.getElementById("warning").style.display = "none";
-    u = document.getElementById("url");
-    u.setAttribute("value", url);
-    if (
-      /www.tripadvisor.com.*\/(Hotel_Review-|Restaurant_Review-)/g.test(url)
-    ) {
-      document.getElementById("logo").src = "images/TripAdvisor.svg";
-    } else if (/www.yelp.com.*/g.test(url)) {
-      document.getElementById("logo").src = "images/Yelp.png";
-      document.getElementById("logo").width = "100";
+
+
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url;
+    var patt = /(www.tripadvisor.com.*\/(Hotel_Review-|Restaurant_Review-))|(www.yelp.com.*)/g;
+    if(patt.test(url)){
+        document.getElementById("warning").style
+        u = document.getElementById("url");
+        u.setAttribute("value", url);
+        if(/www.tripadvisor.com.*\/(Hotel_Review-|Restaurant_Review-)/g.test(url)){
+            document.getElementById("logo").src = "images/TripAdvisor.svg";
+        }else if(/www.yelp.com.*/g.test(url)){
+            document.getElementById("logo").src = "images/Yelp.png";
+            document.getElementById("logo").width = "100";
+        }
+    }else{
+        document.getElementById("submit").style.display="none";
+        document.getElementById("tagline").style.display="none";
+        document.getElementById("logo").style.display="none";
+        // document.getElementById("warn").innerHTML = "<h2> No the TripAdvisor Hotel/Restaurant Website. </h2>";
+        document.getElementById("warning").innerHTML = "This page doesn't appear to be TripAdvisor's Hotel/Restaurant. ";
     }
-  } else {
-    document.getElementById("submit").style.display = "none";
-    document.getElementById("tagline").style.display = "none";
-    document.getElementById("logo").style.display = "none";
-    // document.getElementById("warn").innerHTML = "<h2> No the TripAdvisor Hotel/Restaurant Website. </h2>";
-    document.getElementById("warning").innerHTML =
-      "This page doesn't appear to be TripAdvisor's Hotel/Restaurant. ";
-  }
-});
+  });
